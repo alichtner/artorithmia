7dd5c5300bf4f0ff607fbb5ad697f8b50633ef4a
Aaron Lichtner <aaron@Aarons-MacBook-Pro.local>
2016-07-25 21:50:06 -0700

    script to download drizl cloudinary collection and started the project journal

--- Art_class.py
+++ Art_class.py
@@ -4,36 +4,53 @@ from scipy import misc
 from collections import Counter
 from colorutils import Color
 from colorthief import ColorThief
+import seaborn as sns
+import cv2
 import os
 
 
 class Art(object):
-    """docstring for """
+    """
+    Create an Art object and build a set of attributes and methods
+    about the Art itself
+    """
     def __init__(self):
         self.filename = None
-        self.image_array = None
+        self.short_name = None
+        self.image = None
         self.r_hist = None
         self.g_hist = None
         self.b_hist = None
+        self.bluriness = None
 
     def load_image(self, filename):
         self.filename = filename
-        self.image_array = misc.imread(filename)
+        self.image = misc.imread(filename)
+        self.short_name = self.filename.split('/')[-1].split('.')[0]
         return None
 
     def show_image(self):
-        plt.imshow(self.image_array)
+        plt.imshow(self.image)
         plt.show()
         return None
 
     def extract_colors(self, n_colors=5):
         pass
 
-    def plot_rgb(self):
+    def extract_blur(self):
+        self.bluriness = cv2.Laplacian(self.image, cv2.CV_64F).var()
+
+    def extract_symmetry(self):
+        pass
+
+    def extract_movement(self):
+        pass
+
+    def extract_rgb(self):
         # count rgb values
-        r = Counter(self.image_array[:, :, 0].flatten())
-        g = Counter(self.image_array[:, :, 1].flatten())
-        b = Counter(self.image_array[:, :, 2].flatten())
+        r = Counter(self.image[:, :, 0].flatten())
+        g = Counter(self.image[:, :, 1].flatten())
+        b = Counter(self.image[:, :, 2].flatten())
         self.r_hist = {k: r[k] for k in range(256)}
         self.g_hist = {k: g[k] for k in range(256)}
         self.b_hist = {k: b[k] for k in range(256)}
@@ -50,8 +67,27 @@ class Art(object):
         ax3.set_xlabel('Blue')
         plt.show()
 
+    def get_palette(self, color_count=6, plot=False):
+        color_thief = ColorThief(self.filename)
+        # get the dominant color
+        dominant_color = color_thief.get_color(quality=1)
+        palette = color_thief.get_palette(color_count=color_count)
+        if plot is True:
+            fname = 'plots/' + self.short_name + '_palette.png'
+            my_cols = []
+            for col in palette:
+                c = Color(col)
+                my_cols.append(c.hex)
+            sns.palplot(sns.color_palette(my_cols))
+            plt.title(str(self.short_name) + '_Palette')
+            plt.savefig(fname)
+
 
 if __name__ == '__main__':
-    art = Art()
-    art.load_image('images/cats.png')
-    art.plot_rgb()
+    images = ['images/cats.png', 'images/abstract.jpg', 'images/realism.jpg']
+    for i in images:
+        art = Art()
+        art.load_image(i)
+        art.extract_blur()
+        print art.short_name, ' blurriness is: ', art.bluriness
+        art.get_palette(plot=True)
---
+++ my_process.md
@@ -0,0 +1,12 @@
+# Journal for Discover-Art
+
+## Day 1
+Today was spent validating some of the ideas I will be using to cluster, discover and recommend art for Drizl. The day was mainly focused on starting to build up the pipeline needed to get features for artwork. I explored different packages which can retrieve image characterists (PIL, skimage, misc, openCV, color, colorthief...). Some of the features I'd like to build are: color palette, color temperature, blurriness, symmetry, complexity. 
+
+- #### **Tasks Accomplished:**
+  - started build of Art() class
+    - `extract_RGB`
+    - `extract_blurriness`
+    - `get_palette`
+  - downloaded Drizl Corpus locally
+  - explored how to calculate symmetry
