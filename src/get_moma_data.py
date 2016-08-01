baa021de5c1ad0b5fd12604eca77ed48ee63a012
Aaron Lichtner <aaron@Aarons-MacBook-Pro.local>
2016-08-01 12:17:46 -0700

    added ability to reset the index of the dataframe and check for nan values in URL

--- src/get_moma_data.py
+++ src/get_moma_data.py
@@ -27,6 +27,7 @@ def download_moma_collection(csv_loc='data/moma_collection.csv',
     df = df[df['Department'].isin(['Media and Performance Art',
                                    'Fluxus Collection', 'Film',
                                    'Architecture & Design - Image Archive']) == False]
+    df = df.reset_index()
 
     # create output message for user
     output = """
@@ -43,27 +44,29 @@ def download_moma_collection(csv_loc='data/moma_collection.csv',
         time.sleep(0)
         sys.stdout.write("\r  -- %d of %d Works -- %d %% Artwork Downloaded -- " % (i, len(df), 1.*i/len(df)*100))
         sys.stdout.flush()
-        try:
-            # grab the URL
-            url = df['URL'][i]
-            html = requests.get(url)
-            soup = BeautifulSoup(html.content, 'lxml')
-            # get the image URL, the index value [res] right before the final
-            # split tells the script which resolution image to take 1 ~ 640px width
-            image = soup.findAll('img', attrs={'class':'sov-hero__image-container__image'})[0].get('srcset').split(',')[res].split()[0]
-            image_url = 'http://www.moma.org' + image
-            uopen = urllib2.urlopen(image_url)
-            stream = uopen.read()
 
-            # save the file
-            fname = location + 'moma_' + str(df['ObjectID'][i]) + '.jpg'
-            file = open(fname, 'w')
-            file.write(stream)
-            file.close()
-        except IndexError:
-            print 'No image URL'
+        # grab the URL
+        if df['URL'][i] != 'nan':
+            try:
+                url = df['URL'][i]
+                html = requests.get(url)
+                soup = BeautifulSoup(html.content, 'lxml')
+                # get the image URL, the index value [res] right before the final
+                # split tells the script which resolution image to take 1 ~ 640px width
+                image = soup.findAll('img', attrs={'class':'sov-hero__image-container__image'})[0].get('srcset').split(',')[res].split()[0]
+                image_url = 'http://www.moma.org' + image
+                uopen = urllib2.urlopen(image_url)
+                stream = uopen.read()
+
+                # save the file
+                fname = location + 'moma_' + str(df['ObjectID'][i]) + '.jpg'
+                file = open(fname, 'w')
+                file.write(stream)
+                file.close()
+            except IndexError:
+                print 'No image URL'
     print '\n\n    Download was successful!!!\n'
 
 if __name__ == '__main__':
-    get_moma_collection()
+    #get_moma_collection()
     download_moma_collection()
