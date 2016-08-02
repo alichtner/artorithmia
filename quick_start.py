95fe0a061eb617a9be7b359b74c4479ce9cd362c
Ubuntu <ubuntu@ip-172-31-26-221.us-west-2.compute.internal>
2016-08-02 16:16:17 +0000

    fix quickstart

--- quick_start.py
+++ quick_start.py
@@ -15,7 +15,7 @@ c = ClusterArt()
 if sys.argv[1] == 'shard':
     c.load_collection_from_json('data/Artwork.json', 'collections/shard/')
 else:
-    c.load_collection_from_json('data/Artwork.json', argv[1])
+    c.load_collection_from_json('data/Artwork.json', sys.argv[1])
     c.load_collection_from_json('data/Artwork.json', 'collections/drizl/')
 c.run()
 
