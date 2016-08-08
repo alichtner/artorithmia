0cd64fc85ddd4a7a087876872f6e797dd8811454
Aaron Lichtner <alichtner@gmail.com>
2016-08-08 18:07:30 +0000

    changed to threaded = 1 and debug = 0

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ if __name__ == '__main__':
 
     rec = gl.load_model('data/recommender')   # not sure why this needs the 'app'
     app.secret_key = 'Hjadfjlji1909389283'
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, threaded=True)
